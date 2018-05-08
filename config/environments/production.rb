c41770def48cc2993a9b65f370de128c3d84be03
Nic Devlin <nic@nicdevlin.com.au>
2018-05-08 16:43:06 +1000

    added email config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,17 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: 'coinbay',
+    domain: 'cynicindustries.net',
+    password: ENV['EXCHANGE_PASS'],
+    address: 'mail.cynicindustries.net',
+    port: 587,
+    authentication: :ntlm,
+    enable_starttls_auto: true
+   }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
