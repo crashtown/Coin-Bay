b390be6dbe2380515c81074b411bc7b4aa500d87
Nic Devlin <nic@nicdevlin.com.au>
2018-05-10 16:01:34 +1000

    added seeds file for required models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+
+Category.create!([{
+  name: "Goods"
+},
+{
+  name: "Services"
+}])
+
+p "Created #{Category.count} categories"
+
+Shipping.destroy_all
+
+Shipping.create!([{
+  name: "AusPost Express"
+},
+{
+  name: "AusPost Registered"
+},
+{
+  name: "Digital Only"
+},
+{
+  name: "Pickup Only"
+}])
+
+p "Created #{Shipping.count} shipping types"
+
+Btcprice.destroy_all
+
+Btcprice.create!([{
+  price: "0"
+}])
+
+p "Created #{Btcprice.count} price"
